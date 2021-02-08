4576df959c1133743279636a942b8a6a2e53a6ad
santaCoding <landreax100@gmail.com>
2021-02-08 19:30:35 +0200

    .

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import telebot
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import requests
 
 bot = telebot.TeleBot('')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Начинаем')
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет привет')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Пока пока')
+@bot.message_handler(content_types = ['text'])
+def main(message):
+    pass
+
+
+
 
 bot.polling()
