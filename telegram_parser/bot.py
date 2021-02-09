149a9f187cf910ab3c8a64913114aef4c8ee8f9b
santaCoding <landreax100@gmail.com>
2021-02-09 12:38:33 +0200

    .

--- telegram_parser/bot.py
+++ telegram_parser/bot.py
@@ -1,24 +1,17 @@
 import configparser
 import json
-
 from telethon.sync import TelegramClient
 from telethon import connection
-
-# для корректного переноса времени сообщений в json
 from datetime import date, datetime
 
-# классы для работы с каналами
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
 
-# класс для работы с сообщениями
 from telethon.tl.functions.messages import GetHistoryRequest
 
-# Считываем учетные данные
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-# Присваиваем значения внутренним переменным
 api_id   = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
@@ -30,10 +23,10 @@ client.start()
 
 async def dump_all_participants(channel):
 	"""Записывает json-файл с информацией о всех участниках канала/чата"""
-	offset_user = 0    # номер участника, с которого начинается считывание
-	limit_user = 100   # максимальное число записей, передаваемых за один раз
+	offset_user = 0
+	limit_user = 100
 
-	all_participants = []   # список всех участников канала
+	all_participants = []
 	filter_user = ChannelParticipantsSearch('')
 
 	while True:
@@ -44,7 +37,7 @@ async def dump_all_participants(channel):
 		all_participants.extend(participants.users)
 		offset_user += len(participants.users)
 
-	all_users_details = []   # список словарей с интересующими параметрами участников канала
+	all_users_details = []
 
 	for participant in all_participants:
 		all_users_details.append({"id": participant.id,
@@ -60,12 +53,12 @@ async def dump_all_participants(channel):
 
 async def dump_all_messages(channel):
 	"""Записывает json-файл с информацией о всех сообщениях канала/чата"""
-	offset_msg = 0    # номер записи, с которой начинается считывание
-	limit_msg = 100   # максимальное число записей, передаваемых за один раз
+	offset_msg = 0
+	limit_msg = 100
 
-	all_messages = []   # список всех сообщений
+	all_messages = []
 	total_messages = 0
-	total_count_limit = 0  # поменяйте это значение, если вам нужны не все сообщения
+	total_count_limit = 0
 
 	class DateTimeEncoder(json.JSONEncoder):
 		'''Класс для сериализации записи дат в JSON'''
